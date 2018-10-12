e5fa8160afab8584533ba0043606bccc8b0766ac
Aayush Thakur <thakur.aayush@hotmail.com>
2018-10-12 18:35:56 +0530

    Reduced gas send during a transaction

--- truffle-config.js
+++ truffle-config.js
@@ -47,10 +47,10 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-      gas: 8000000,           // Gas sent with each transaction (default: ~6700000)
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 4000000,           // Gas sent with each transaction (default: ~6700000)
     },
 
     // Another network with more advanced options...
@@ -93,11 +93,11 @@ module.exports = {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
-       optimizer: {
-         enabled: false,
-         runs: 200
-       },
-       evmVersion: "byzantium"
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
       }
     }
   }
